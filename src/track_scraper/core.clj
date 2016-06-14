ce7c43b7c5c5269f11d4908c16690889dc0d5d54
TheoLeanse <theoleanse@gmail.com>
2016-06-14 15:36:37 +0100

    remove fluff

--- src/track_scraper/core.clj
+++ src/track_scraper/core.clj
@@ -3,12 +3,8 @@
             [clojure.string :as str]))
 
 (def ^:dynamic *base-url* "https://djfriendly.co.uk/stocklist.php?show=")
-(def ^:dynamic *selectors* [:tr :> :td :> :span])
 (def ^:dynamic *artist-selectors* [:tr :> :td :> :span.text13b])
 (def ^:dynamic *album-selectors* [:tr :> :td :> :span.text13])
-
-(def ^:dynamic *general-selectors* [:td :> :table])
-
 (def genres ["arthurrussell", "Gospel", "electronic", "jazz", "soulfunk", "singles", "dsingles", "disco", "discouk", "tbreak", "disconotdisco", "Italo", "danceclassic", "afro", "afro12", "latin", "brazil", "soundtracks", "breaks", "funky", "oldrap", "electro", "rap", "raplp", "chicago", "detroit", "newyork", "ukhouse", "Mod/Latin", "crossover", "Group/Bal", "Jazz7s", "Tropical", "reggaenot", "NSSingles", "MTown", "5060soul", "gogo", "spokenword", "reggae", "library", "rock", "TRO"])
 
 (defn compose-url [genre]
