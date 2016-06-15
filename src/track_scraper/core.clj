35040bb433e096e793ea822b2fe4c2c73b94042e
TheoLeanse <theoleanse@gmail.com>
2016-06-15 14:19:22 +0100

    dsicogs

--- src/track_scraper/core.clj
+++ src/track_scraper/core.clj
@@ -3,8 +3,10 @@
             [clojure.string :as str]))
 
 (def ^:dynamic *base-url* "https://djfriendly.co.uk/stocklist.php?show=")
+
 (def ^:dynamic *artist-selectors* [:tr :> :td :> :span.text13b])
 (def ^:dynamic *album-selectors* [:tr :> :td :> :span.text13])
+
 (def genres ["arthurrussell", "Gospel", "electronic", "jazz", "soulfunk", "singles", "dsingles", "disco", "discouk", "tbreak", "disconotdisco", "Italo", "danceclassic", "afro", "afro12", "latin", "brazil", "soundtracks", "breaks", "funky", "oldrap", "electro", "rap", "raplp", "chicago", "detroit", "newyork", "ukhouse", "Mod/Latin", "crossover", "Group/Bal", "Jazz7s", "Tropical", "reggaenot", "NSSingles", "MTown", "5060soul", "gogo", "spokenword", "reggae", "library", "rock", "TRO"])
 
 (defn compose-url [genre]
@@ -22,4 +24,19 @@
   (map vector (records url *artist-selectors*)
        (records url *album-selectors*)))
 
-(map zip-artist-album urls)
+(def artist-album-vectors (map zip-artist-album urls))
+
+(defn discogs-search
+  [artist-album]
+  (str "https://api.discogs.com/database/search?title=" (apply str artist-album) "&token=" (System/getenv "DISCOGS_TOKEN")))
+
+(defn genre-strings [list-of-vectors]
+  (map discogs-search list-of-vectors))
+
+(def search-urls-by-section (map genre-strings artist-album-vectors))
+
+(defn search-discogs [url]
+  ;; TODO
+  (url))
+
+(map #(map search-discogs %) search-urls-by-section)
